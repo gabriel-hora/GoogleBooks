233c8d65f8fda066d2fc58c31f6c725319a38cb7
Gabriel Hora <gabrielhoradeveloper@gmail.com>
2022-07-11 22:28:54 -0300

    implementation of Fragments

--- app/src/main/java/com/example/googlebooks/ui/MainActivity.kt
+++ app/src/main/java/com/example/googlebooks/ui/MainActivity.kt
@@ -1,32 +1,16 @@
 package com.example.googlebooks.ui
 
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Toast
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.lifecycleScope
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.googlebooks.R
+import com.example.googlebooks.databinding.ActivityBookDetailBinding.inflate
 import com.example.googlebooks.databinding.ActivityMainBinding
-import com.example.googlebooks.model.BookHttp
-import com.example.googlebooks.model.Volume
-import com.example.googlebooks.ui.adapter.BookListAdapter
-import com.example.googlebooks.ui.viewModel.BookListViewModel
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import com.example.googlebooks.ui.fragments.BookListFragment
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    private val viewModel: BookListViewModel by lazy {
-        ViewModelProvider(this)[BookListViewModel::class.java]
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,34 +18,5 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
         val context = this
-
-        binding.recyclerView.layoutManager = LinearLayoutManager(this)
-
-        viewModel.state.observe(this, Observer { state ->
-            when(state) {
-                is BookListViewModel.State.Loading -> {
-                    //Não está resgatando o atributo correto
-                    binding.vwLoading.visibility = View.VISIBLE
-                }
-                is BookListViewModel.State.Loaded -> {
-                    binding.vwLoading.visibility = View.GONE
-                    binding.recyclerView.adapter = BookListAdapter(state.items, context, this@MainActivity::openBookDetail)
-                }
-                is BookListViewModel.State.Error -> {
-                    binding.vwLoading.visibility = View.GONE
-                    if(!state.hasConsumed) {
-                        state.hasConsumed = true
-                        Toast.makeText(this@MainActivity, R.string.error_loading, Toast.LENGTH_LONG).show()
-                    }
-                }
-            }
-        })
-        viewModel.loadBooks()
-    }
-
-    private fun openBookDetail(volume: Volume) {
-        val detailIntent = Intent(this, BookDetailActivity::class.java)
-        detailIntent.putExtra("book", volume)
-        startActivity(detailIntent)
     }
 }
---
+++ app/src/main/java/com/example/googlebooks/ui/fragments/BookListFragment.kt
@@ -0,0 +1,75 @@
+package com.example.googlebooks.ui.fragments
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.googlebooks.R
+import com.example.googlebooks.databinding.FragmentBookListBinding
+import com.example.googlebooks.model.Volume
+import com.example.googlebooks.ui.BookDetailActivity
+import com.example.googlebooks.ui.adapter.BookListAdapter
+import com.example.googlebooks.ui.viewModel.BookListViewModel
+
+class BookListFragment: Fragment() {
+
+    private var _binding: FragmentBookListBinding? = null
+    private val binding get() = _binding!!
+
+    private val viewModel: BookListViewModel by lazy {
+        ViewModelProvider(this)[BookListViewModel::class.java]
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentBookListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        viewModel.state.observe(viewLifecycleOwner, Observer { state ->
+            when(state) {
+                is BookListViewModel.State.Loading -> {
+                    //Não está resgatando o atributo correto
+                    binding.vwLoading.visibility = View.VISIBLE
+                }
+                is BookListViewModel.State.Loaded -> {
+                    binding.vwLoading.visibility = View.GONE
+                    binding.recyclerView.adapter = BookListAdapter(state.items, requireContext(), this::openBookDetail)
+                }
+                is BookListViewModel.State.Error -> {
+                    binding.vwLoading.visibility = View.GONE
+                    if(!state.hasConsumed) {
+                        state.hasConsumed = true
+                        Toast.makeText(requireContext(), R.string.error_loading, Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+        })
+        viewModel.loadBooks()
+    }
+
+    private fun openBookDetail(volume: Volume) {
+        val detailIntent = Intent(requireContext(), BookDetailActivity::class.java)
+        detailIntent.putExtra("book", volume)
+        startActivity(detailIntent)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
